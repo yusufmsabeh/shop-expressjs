3bad4df26dbc650eb4f2923f647e31088667a6c5
youms <yussef.msabeh@gmail.com>
2023-02-14 10:55:06 +0200

    build: add public folder to the statics files

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //External Imports
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const ejs = require("ejs");
 // My Imports
 const shopRouter = require("./routes/shop");
 
 const app = express();
-
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRouter);
 app.listen(3000);
