835514e3039646c289d0bd0e4512d81b6fe03e72
youms <yussef.msabeh@gmail.com>
2023-02-14 10:28:36 +0200

    feat: handle index request

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+//External Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// My Imports
+const shopRouter = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(shopRouter);
+app.listen(3000);
