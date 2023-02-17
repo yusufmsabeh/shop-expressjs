7e8ec40bb273da3af901ef0af1b982fac202268c
youms <yussef.msabeh@gmail.com>
2023-02-17 15:55:07 +0200

    feat: add get add products and get product details handlers and thier routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-
+const productDao = require("../data/DAOs/product-dao");
 exports.getIndex = async (request, response, next) => {
   try {
     response.send("<h1>Home page<h1>");
@@ -9,10 +9,50 @@ exports.getIndex = async (request, response, next) => {
 };
 exports.getProducts = async (request, response, next) => {
   try {
+    const products = await productDao.fetchProducts();
     response.render("user/products", {
       pageTitle: "Products",
       path: "/products",
-      products: [],
+      products: products || [],
+    });
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+};
+exports.postAddProducts = async (request, response, next) => {
+  try {
+    const product = request.body;
+    product.userId = 1;
+    await productDao.insertProduct(product);
+    response.redirect("/admin/add-product");
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+};
+
+exports.getAddProducts = async (request, response, next) => {
+  try {
+    response.render("user/add-product", {
+      pageTitle: "Add Product",
+      path: "admin/add-product",
+    });
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+};
+
+exports.getProductDetails = async (request, response, next) => {
+  try {
+    const productId = request.params.id;
+
+    const product = await productDao.getProductById(productId);
+    response.render("user/product-details", {
+      pageTitle: product.title || "UNKNOWN",
+      path: "none",
+      product: product,
     });
   } catch (e) {
     console.error(e);
--- routes/shop.js
+++ routes/shop.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
+router.get("/admin/add-product", shopController.getAddProducts);
+router.post("/admin/add-product", shopController.postAddProducts);
+router.get("/product-details/:id", shopController.getProductDetails);
 module.exports = router;
