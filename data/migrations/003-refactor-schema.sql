9678759cb0827d52bf218eebda79c2a1392730d5
youms <yussef.msabeh@gmail.com>
2023-03-02 18:30:45 +0200

    fix: refactor the database schema

---
+++ data/migrations/003-refactor-schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE order_items;
+DROP TABLE orders;
+
+CREATE TABLE orders (
+    id VARCHAR PRIMARY KEY NOT NULL,
+    user_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR NOT NULL,
+    product_id VARCHAR NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
