a348db267fc3b85e067e794799471d966ff7ee34
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-03-21 14:54:21 +0200

    fear: assign validators middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const authController = require("../controllers/auth");
-
+const loginRequestValidator = require("../validation/login-request-validation");
+const signupRequestValidator = require("../validation/signup-request-validation");
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
+router.post("/login", loginRequestValidator, authController.postLogin);
 
 router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.post("/signup", signupRequestValidator, authController.postSignup);
 
 router.get("/logout", authController.getLogout);
 
